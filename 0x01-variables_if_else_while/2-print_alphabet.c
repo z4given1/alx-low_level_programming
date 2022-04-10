dc35ed483a3ef306532ed8951ffe425b4be48dbd
z4given1 <z4given1@gmail.com>
2022-04-10 02:52:22 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,15 +2,15 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success/correct)
+ * Return: Always 0
  */
 int main(void)
 {
-	char alphabet:
-		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		{
-			putchar(alphabet);
-		}
+	char alli;
+	for (alli = 'a'; alli <= 'z'; alli++)
+	{
+		putchar(alli);
+	}
 	putchar('\n');
 	return (0);
 }
