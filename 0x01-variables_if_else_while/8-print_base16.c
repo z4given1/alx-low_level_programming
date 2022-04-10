8941f827bd2406954eb598ec7934b12d6cca543d
z4given1 <z4given1@gmail.com>
2022-04-10 04:18:33 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char k;
+
+	for (k = 48; k <= 57; k++)
+	{
+		putchar(k);
+	}
+	for (k = 97; k <= 102; k++)
+	{
+		putchar(k);
+	}
+	putchar('\n');
+	return (0);
+}
