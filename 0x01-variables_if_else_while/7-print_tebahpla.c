27559701e044e1c0e10f785336c950f5b18aecfb
z4given1 <z4given1@gmail.com>
2022-04-10 04:08:04 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -E
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char qu;
+
+	for (qu == 'z'; qu >= 'a'; qu--)
+	{
+		putchar(qu);
+	}
+	putchar('\n');
+	return (0);
+}
