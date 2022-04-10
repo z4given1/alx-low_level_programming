9cf329dd8ee40a7e11b756bc55abdc1cef871697
z4given1 <z4given1@gmail.com>
2022-04-10 09:48:11 -0700

    po

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (success/correct)
- *     */
+ *main - Entry point
+ *
+ *Return: Always 0 (success/correct)
+ */
 int main(void)
 
 
