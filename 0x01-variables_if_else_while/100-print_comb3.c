bb0b95129124d5ca8ce94a6d7e765fb7225c36e3
z4given1 <z4given1@gmail.com>
2022-04-10 08:45:46 -0700

    kl

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,13 @@ int main(void)
 	int i;
 	int k;
 
-	for (i = 48; i <= 78; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		for (k = i+1; k <= 78; k++)
+		for (k = i+1; k <= 57; k++)
 		{
 			putchar(i);
 			putchar(k);
-			if ((i == 59) && (k == 57))
+			if ((i==k)||(i == 47) && (k == 57))
 			{
 				break;
 			}
