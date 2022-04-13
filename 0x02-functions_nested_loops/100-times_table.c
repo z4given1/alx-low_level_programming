f49220b02de7aca2f94bf28971efe1e194b039ad
z4given1 <z4given1@gmail.com>
2022-04-13 10:47:09 +0300

    H

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,9 +22,13 @@ void print_times_table(int n)
 				prod = num * mult;
 
 				if (prod <= 99)
+				{
 					_putchar(' ');
+				}
 				if (prod <= 9)
+				{
 					_putchar(' ');
+				}
 
 				if (prod >= 100)
 				{
