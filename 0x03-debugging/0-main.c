db1f74cbdcbb4515912c9c65d9c0db966bccf71e
z4given1 <z4given1@gmail.com>
2022-04-14 17:42:12 +0300

    0-main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * main - main
- *
  * Return: 0
  */
+
 int main(void)
 {
-int i;
+	int i;
 
-i = 0;
-positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-return (0);
-
+	return (0);
 }
