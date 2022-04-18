8526e77ef63d9f0a19b9bc889f84e722a560f2aa
z4given1 <z4given1@gmail.com>
2022-04-18 16:48:28 +0300

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the value of two int
+ *@a: 1st int
+ *@b: 2nd int
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = 0;
+	c = *a;
+	*a = *b;
+	*b = *c;
+}
